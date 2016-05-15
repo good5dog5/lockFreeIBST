9861f15b1ce843a8be6adb7dac5eb484d9e14f7c
Jordan Huang <good5dog5@gmail.com>
2016-05-15 11:05:37 -0400

    Make indent space consistently and replace tab with 4 spaces.

--- src/TestLockFree.cpp
+++ src/TestLockFree.cpp
@@ -13,214 +13,212 @@ struct timespec runTime,transientTime;
 
 static inline unsigned long getRandom(gsl_rng* r)
 {
-	return(gsl_rng_uniform_int(r,keyRange) + 2);
+    return(gsl_rng_uniform_int(r,keyRange) + 2);
 }
 
 void *operateOnTree(void* tArgs)
 {
-  int chooseOperation;
-  unsigned long lseed;
-	unsigned long key;
-  struct tArgs* tData = (struct tArgs*) tArgs;
-  const gsl_rng_type* T;
-  gsl_rng* r;
-  gsl_rng_env_setup();
-  T = gsl_rng_default;
-  r = gsl_rng_alloc(T);
-	lseed = tData->lseed;
-  gsl_rng_set(r,lseed);
+    int chooseOperation;
+    unsigned long lseed;
+    unsigned long key;
+    struct tArgs* tData = (struct tArgs*) tArgs;
+    const gsl_rng_type* T;
+    gsl_rng* r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);
+    lseed = tData->lseed;
+    gsl_rng_set(r,lseed);
 
-	tData->newNode=NULL;
-  tData->isNewNodeAvailable=false;
-	tData->readCount=0;
-  tData->successfulReads=0;
-  tData->unsuccessfulReads=0;
-  tData->readRetries=0;
-  tData->insertCount=0;
-  tData->successfulInserts=0;
-  tData->unsuccessfulInserts=0;
-  tData->insertRetries=0;
-  tData->deleteCount=0;
-  tData->successfulDeletes=0;
-  tData->unsuccessfulDeletes=0;
-  tData->deleteRetries=0;
-	tData->seekRetries=0;
-	tData->seekLength=0;
+    tData->newNode=NULL;
+    tData->isNewNodeAvailable=false;
+    tData->readCount=0;
+    tData->successfulReads=0;
+    tData->unsuccessfulReads=0;
+    tData->readRetries=0;
+    tData->insertCount=0;
+    tData->successfulInserts=0;
+    tData->unsuccessfulInserts=0;
+    tData->insertRetries=0;
+    tData->deleteCount=0;
+    tData->successfulDeletes=0;
+    tData->unsuccessfulDeletes=0;
+    tData->deleteRetries=0;
+    tData->seekRetries=0;
+    tData->seekLength=0;
 
-  while(!start)
-  {
-  }
-	
-	while(!steadyState)
-	{
-	  chooseOperation = gsl_rng_uniform(r)*100;
-		key = gsl_rng_uniform_int(r,keyRange) + 2;
-    if(chooseOperation < findPercent)
-    {
-      search(tData, key);
-    }
-    else if (chooseOperation < insertPercent)
-    {
-      insert(tData, key);
-    }
-    else
-    {
-      remove(tData, key);
-    }
-	}
-	
-  tData->readCount=0;
-  tData->successfulReads=0;
-  tData->unsuccessfulReads=0;
-  tData->readRetries=0;
-  tData->insertCount=0;
-  tData->successfulInserts=0;
-  tData->unsuccessfulInserts=0;
-  tData->insertRetries=0;
-  tData->deleteCount=0;
-  tData->successfulDeletes=0;
-  tData->unsuccessfulDeletes=0;
-  tData->deleteRetries=0;
-	tData->seekRetries=0;
-	tData->seekLength=0;
-	
-	while(!stop)
-  {
-    chooseOperation = gsl_rng_uniform(r)*100;
-		key = gsl_rng_uniform_int(r,keyRange) + 2;
+    while(!start) {}
 
-    if(chooseOperation < findPercent)
+    while(!steadyState)
     {
-			tData->readCount++;
-      search(tData, key);
+        chooseOperation = gsl_rng_uniform(r)*100;
+        key = gsl_rng_uniform_int(r,keyRange) + 2;
+        if(chooseOperation < findPercent)
+        {
+            search(tData, key);
+        }
+        else if (chooseOperation < insertPercent)
+        {
+            insert(tData, key);
+        }
+        else
+        {
+            remove(tData, key);
+        }
     }
-    else if (chooseOperation < insertPercent)
+
+    tData->readCount=0;
+    tData->successfulReads=0;
+    tData->unsuccessfulReads=0;
+    tData->readRetries=0;
+    tData->insertCount=0;
+    tData->successfulInserts=0;
+    tData->unsuccessfulInserts=0;
+    tData->insertRetries=0;
+    tData->deleteCount=0;
+    tData->successfulDeletes=0;
+    tData->unsuccessfulDeletes=0;
+    tData->deleteRetries=0;
+    tData->seekRetries=0;
+    tData->seekLength=0;
+
+    while(!stop)
     {
-			tData->insertCount++;
-      insert(tData, key);
+        chooseOperation = gsl_rng_uniform(r)*100;
+        key = gsl_rng_uniform_int(r,keyRange) + 2;
+
+        if(chooseOperation < findPercent)
+        {
+            tData->readCount++;
+            search(tData, key);
+        }
+        else if (chooseOperation < insertPercent)
+        {
+            tData->insertCount++;
+            insert(tData, key);
+        }
+        else
+        {
+            tData->deleteCount++;
+            remove(tData, key);
+        }
     }
-    else
-    {
-			tData->deleteCount++;
-      remove(tData, key);
-    }
-  }
-  return NULL;
+    return NULL;
 }
 struct tArgs** tArgs;
 
 int main(int argc, char *argv[])
 {
-	unsigned long lseed;
-	//get run configuration from command line
-  NUM_OF_THREADS = atoi(argv[1]);
-  findPercent = atoi(argv[2]);
-  insertPercent= findPercent + atoi(argv[3]);
-  deletePercent = insertPercent + atoi(argv[4]);
+    unsigned long lseed;
+    //get run configuration from command line
+    NUM_OF_THREADS = atoi(argv[1]);
+    findPercent = atoi(argv[2]);
+    insertPercent= findPercent + atoi(argv[3]);
+    deletePercent = insertPercent + atoi(argv[4]);
 
-	runTime.tv_sec = atoi(argv[5]);
-	runTime.tv_nsec =0;
-	transientTime.tv_sec=0;
-	transientTime.tv_nsec=2000000;
+    runTime.tv_sec = atoi(argv[5]);
+    runTime.tv_nsec =0;
+    transientTime.tv_sec=0;
+    transientTime.tv_nsec=2000000;
 
-  keyRange = (unsigned long) atol(argv[6])-1;
-	lseed = (unsigned long) atol(argv[7]);
+    keyRange = (unsigned long) atol(argv[6])-1;
+    lseed = (unsigned long) atol(argv[7]);
 
-  tArgs = (struct tArgs**) malloc(NUM_OF_THREADS * sizeof(struct tArgs*)); 
+    tArgs = (struct tArgs**) malloc(NUM_OF_THREADS * sizeof(struct tArgs*)); 
 
-  const gsl_rng_type* T;
-  gsl_rng* r;
-  gsl_rng_env_setup();
-  T = gsl_rng_default;
-  r = gsl_rng_alloc(T);
-  gsl_rng_set(r,lseed);
-	
-  createHeadNodes(); //Initialize the tree. Must be called before doing any operations on the tree
-  
-  struct tArgs* initialInsertArgs = (struct tArgs*) malloc(sizeof(struct tArgs));
-  initialInsertArgs->successfulInserts=0;
-	initialInsertArgs->newNode=NULL;
-  initialInsertArgs->isNewNodeAvailable=false;
-	
-  while(initialInsertArgs->successfulInserts < keyRange/2) //populate the tree with 50% of keys
-  {
-    insert(initialInsertArgs,gsl_rng_uniform_int(r,keyRange) + 2);
-  }
-  pthread_t threadArray[NUM_OF_THREADS];
-  for(int i=0;i<NUM_OF_THREADS;i++)
-  {
-    tArgs[i] = (struct tArgs*) malloc(sizeof(struct tArgs));
-    tArgs[i]->tId = i;
-    tArgs[i]->lseed = gsl_rng_get(r);
-  }
+    const gsl_rng_type* T;
+    gsl_rng* r;
+    gsl_rng_env_setup();
+    T = gsl_rng_default;
+    r = gsl_rng_alloc(T);
+    gsl_rng_set(r,lseed);
 
-	for(int i=0;i<NUM_OF_THREADS;i++)
-	{
-		pthread_create(&threadArray[i], NULL, operateOnTree, (void*) tArgs[i] );
-	}
-	
-	start=true; 										//start operations
-	nanosleep(&transientTime,NULL); //warmup
-	steadyState=true;
-	nanosleep(&runTime,NULL);
-	stop=true;											//stop operations
-	
-	for(int i=0;i<NUM_OF_THREADS;i++)
-	{
-		pthread_join(threadArray[i], NULL);
-	}	
+    createHeadNodes(); //Initialize the tree. Must be called before doing any operations on the tree
 
-  unsigned long totalReadCount=0;
-  unsigned long totalSuccessfulReads=0;
-  unsigned long totalUnsuccessfulReads=0;
-  unsigned long totalReadRetries=0;
-  unsigned long totalInsertCount=0;
-  unsigned long totalSuccessfulInserts=0;
-  unsigned long totalUnsuccessfulInserts=0;
-  unsigned long totalInsertRetries=0;
-  unsigned long totalDeleteCount=0;
-  unsigned long totalSuccessfulDeletes=0;
-  unsigned long totalUnsuccessfulDeletes=0;
-  unsigned long totalDeleteRetries=0;
-	unsigned long totalSeekRetries=0;
-	unsigned long totalSeekLength=0;
- 
-  for(int i=0;i<NUM_OF_THREADS;i++)
-  {
-    totalReadCount += tArgs[i]->readCount;
-    totalSuccessfulReads += tArgs[i]->successfulReads;
-    totalUnsuccessfulReads += tArgs[i]->unsuccessfulReads;
-    totalReadRetries += tArgs[i]->readRetries;
+    struct tArgs* initialInsertArgs = (struct tArgs*) malloc(sizeof(struct tArgs));
+    initialInsertArgs->successfulInserts=0;
+    initialInsertArgs->newNode=NULL;
+    initialInsertArgs->isNewNodeAvailable=false;
 
-    totalInsertCount += tArgs[i]->insertCount;
-    totalSuccessfulInserts += tArgs[i]->successfulInserts;
-    totalUnsuccessfulInserts += tArgs[i]->unsuccessfulInserts;
-    totalInsertRetries += tArgs[i]->insertRetries;
-    totalDeleteCount += tArgs[i]->deleteCount;
-    totalSuccessfulDeletes += tArgs[i]->successfulDeletes;
-    totalUnsuccessfulDeletes += tArgs[i]->unsuccessfulDeletes;
-    totalDeleteRetries += tArgs[i]->deleteRetries;
-		totalSeekRetries += tArgs[i]->seekRetries;
-		totalSeekLength += tArgs[i]->seekLength;
-  }
-	unsigned long totalOperations = totalReadCount + totalInsertCount + totalDeleteCount;
-	MOPS = totalOperations/(runTime.tv_sec*1000000.0);
-	printf("max key size:%d\n",atoi(argv[6]));
-	printf("find:%d\n",findPercent);
-	printf("insert:%d\n",insertPercent-findPercent);
-	printf("delete:%d\n",deletePercent-insertPercent);
-	printf("num of thread:%d\n",NUM_OF_THREADS);
-	printf("size:%ld\n",size());
-	printf("totalReadCount:%ld\n",totalReadCount);
-	printf("totalInsertCount:%ld\n",totalInsertCount);
-	printf("totalDeleteCount:%ld\n",totalDeleteCount);
-	printf("totalReadRetries:%ld\n",totalReadRetries);
-	printf("totalSeekRetries:%ld\n",totalSeekRetries);
-	printf("totalInsertRetries:%ld\n",totalInsertRetries);
-	printf("totalDeleteRetries:%.2f\n",totalDeleteRetries);
-	printf("length/oper:%.2f\n",totalSeekLength*1.0/totalOperations);
-	printf("Mops:%.2f\n",MOPS);
-	printf("k%d;%d-%d-%d;%d;%ld;%ld;%ld;%ld;%ld;%ld;%ld;%ld;%.2f;%.2f\n",atoi(argv[6]),findPercent,(insertPercent-findPercent),(deletePercent-insertPercent),NUM_OF_THREADS,size(),totalReadCount,totalInsertCount,totalDeleteCount,totalReadRetries,totalSeekRetries,totalInsertRetries,totalDeleteRetries,totalSeekLength*1.0/totalOperations,MOPS);
-	assert(isValidTree());
-	pthread_exit(NULL);
+    while(initialInsertArgs->successfulInserts < keyRange/2) //populate the tree with 50% of keys
+    {
+        insert(initialInsertArgs,gsl_rng_uniform_int(r,keyRange) + 2);
+    }
+    pthread_t threadArray[NUM_OF_THREADS];
+    for(int i=0;i<NUM_OF_THREADS;i++)
+    {
+        tArgs[i] = (struct tArgs*) malloc(sizeof(struct tArgs));
+        tArgs[i]->tId = i;
+        tArgs[i]->lseed = gsl_rng_get(r);
+    }
+
+    for(int i=0;i<NUM_OF_THREADS;i++)
+    {
+        pthread_create(&threadArray[i], NULL, operateOnTree, (void*) tArgs[i] );
+    }
+
+    start=true;                                         //start operations
+    nanosleep(&transientTime,NULL); //warmup
+    steadyState=true;
+    nanosleep(&runTime,NULL);
+    stop=true;                                          //stop operations
+
+    for(int i=0;i<NUM_OF_THREADS;i++)
+    {
+        pthread_join(threadArray[i], NULL);
+    }   
+
+    unsigned long totalReadCount=0;
+    unsigned long totalSuccessfulReads=0;
+    unsigned long totalUnsuccessfulReads=0;
+    unsigned long totalReadRetries=0;
+    unsigned long totalInsertCount=0;
+    unsigned long totalSuccessfulInserts=0;
+    unsigned long totalUnsuccessfulInserts=0;
+    unsigned long totalInsertRetries=0;
+    unsigned long totalDeleteCount=0;
+    unsigned long totalSuccessfulDeletes=0;
+    unsigned long totalUnsuccessfulDeletes=0;
+    unsigned long totalDeleteRetries=0;
+    unsigned long totalSeekRetries=0;
+    unsigned long totalSeekLength=0;
+
+    for(int i=0;i<NUM_OF_THREADS;i++)
+    {
+        totalReadCount += tArgs[i]->readCount;
+        totalSuccessfulReads += tArgs[i]->successfulReads;
+        totalUnsuccessfulReads += tArgs[i]->unsuccessfulReads;
+        totalReadRetries += tArgs[i]->readRetries;
+
+        totalInsertCount += tArgs[i]->insertCount;
+        totalSuccessfulInserts += tArgs[i]->successfulInserts;
+        totalUnsuccessfulInserts += tArgs[i]->unsuccessfulInserts;
+        totalInsertRetries += tArgs[i]->insertRetries;
+        totalDeleteCount += tArgs[i]->deleteCount;
+        totalSuccessfulDeletes += tArgs[i]->successfulDeletes;
+        totalUnsuccessfulDeletes += tArgs[i]->unsuccessfulDeletes;
+        totalDeleteRetries += tArgs[i]->deleteRetries;
+        totalSeekRetries += tArgs[i]->seekRetries;
+        totalSeekLength += tArgs[i]->seekLength;
+    }
+    unsigned long totalOperations = totalReadCount + totalInsertCount + totalDeleteCount;
+    MOPS = totalOperations/(runTime.tv_sec*1000000.0);
+    printf("max key size:%d\n",atoi(argv[6]));
+    printf("find:%d\n",findPercent);
+    printf("insert:%d\n",insertPercent-findPercent);
+    printf("delete:%d\n",deletePercent-insertPercent);
+    printf("num of thread:%d\n",NUM_OF_THREADS);
+    printf("size:%ld\n",size());
+    printf("totalReadCount:%ld\n",totalReadCount);
+    printf("totalInsertCount:%ld\n",totalInsertCount);
+    printf("totalDeleteCount:%ld\n",totalDeleteCount);
+    printf("totalReadRetries:%ld\n",totalReadRetries);
+    printf("totalSeekRetries:%ld\n",totalSeekRetries);
+    printf("totalInsertRetries:%ld\n",totalInsertRetries);
+    printf("totalDeleteRetries:%ld\n",totalDeleteRetries);
+    printf("length/oper:%.2f\n",totalSeekLength*1.0/totalOperations);
+    printf("Mops:%.2f\n",MOPS);
+    printf("k%d;%d-%d-%d;%d;%ld;%ld;%ld;%ld;%ld;%ld;%ld;%ld;%.2f;%.2f\n",atoi(argv[6]),findPercent,(insertPercent-findPercent),(deletePercent-insertPercent),NUM_OF_THREADS,size(),totalReadCount,totalInsertCount,totalDeleteCount,totalReadRetries,totalSeekRetries,totalInsertRetries,totalDeleteRetries,totalSeekLength*1.0/totalOperations,MOPS);
+    assert(isValidTree());
+    pthread_exit(NULL);
 }
--- src/header.h
+++ src/header.h
@@ -26,7 +26,7 @@
 #define INF_S 0x1
 #define INF_T 0x7FFFFFFE
 #define KEY_MASK 0x80000000
-#define ADDRESS_MASK 15	
+#define ADDRESS_MASK 15 
 
 #define NULL_BIT 8
 #define INJECT_BIT 4
@@ -40,74 +40,74 @@ typedef enum {DELETE_FLAG, PROMOTE_FLAG} Flag;
 
 struct node
 {
-	#ifdef TBB
-		tbb::atomic<unsigned long> markAndKey;			//format <markFlag,address>
-		tbb::atomic<node*> child[K];								//format <address,NullBit,InjectFlag,DeleteFlag,PromoteFlag>
-		tbb::atomic<unsigned long> readyToReplace;
-	#else
-		std::atomic<unsigned long> markAndKey;			//format <markFlag,address>
-		std::atomic<node*> child[K];								//format <address,NullBit,InjectFlag,DeleteFlag,PromoteFlag>
-		std::atomic<unsigned long> readyToReplace;
-	#endif
+    #ifdef TBB
+        tbb::atomic<unsigned long> markAndKey;          //format <markFlag,address>
+        tbb::atomic<node*> child[K];                    //format <address,NullBit,InjectFlag,DeleteFlag,PromoteFlag>
+        tbb::atomic<unsigned long> readyToReplace;
+    #else
+        std::atomic<unsigned long> markAndKey;          //format <markFlag,address>
+        std::atomic<node*> child[K];                    //format <address,NullBit,InjectFlag,DeleteFlag,PromoteFlag>
+        std::atomic<unsigned long> readyToReplace;
+    #endif
 };
 
 struct edge
 {
-	struct node* parent;
-	struct node* child;
-	int which;
+    struct node* parent;
+    struct node* child;
+    int which;
 };
 
 struct seekRecord
 {
-	struct edge lastEdge;
-	struct edge pLastEdge;
-	struct edge injectionEdge;
+    struct edge lastEdge;
+    struct edge pLastEdge;
+    struct edge injectionEdge;
 };
 
 struct anchorRecord
 {
-	struct node* node;
-	unsigned long key;
+    struct node* node;
+    unsigned long key;
 };
 
 struct stateRecord
 {
-	int depth;
-	struct edge targetEdge;
-	struct edge pTargetEdge;
-	unsigned long targetKey;
-	unsigned long currentKey;
-	Mode mode;
-	Type type;
-	struct seekRecord successorRecord;
+    int depth;
+    struct edge targetEdge;
+    struct edge pTargetEdge;
+    unsigned long targetKey;
+    unsigned long currentKey;
+    Mode mode;
+    Type type;
+    struct seekRecord successorRecord;
 };
 
 struct tArgs
 {
-	int tId;
-	unsigned long lseed;
-	unsigned long readCount;
-	unsigned long successfulReads;
-	unsigned long unsuccessfulReads;
-	unsigned long readRetries;
-	unsigned long insertCount;
-	unsigned long successfulInserts;
-	unsigned long unsuccessfulInserts;
-	unsigned long insertRetries;
-	unsigned long deleteCount;
-	unsigned long successfulDeletes;
-	unsigned long unsuccessfulDeletes;
-	unsigned long deleteRetries;
-	struct node* newNode;
-	bool isNewNodeAvailable;
-	struct seekRecord targetRecord;
-	struct seekRecord pSeekRecord;
-	struct stateRecord myState;	
-	struct anchorRecord anchorRecord;
-	struct anchorRecord pAnchorRecord;
-	unsigned long seekRetries;
-	unsigned long seekLength;
+    int tId;
+    unsigned long lseed;
+    unsigned long readCount;
+    unsigned long successfulReads;
+    unsigned long unsuccessfulReads;
+    unsigned long readRetries;
+    unsigned long insertCount;
+    unsigned long successfulInserts;
+    unsigned long unsuccessfulInserts;
+    unsigned long insertRetries;
+    unsigned long deleteCount;
+    unsigned long successfulDeletes;
+    unsigned long unsuccessfulDeletes;
+    unsigned long deleteRetries;
+    struct node* newNode;
+    bool isNewNodeAvailable;
+    struct seekRecord targetRecord;
+    struct seekRecord pSeekRecord;
+    struct stateRecord myState; 
+    struct anchorRecord anchorRecord;
+    struct anchorRecord pAnchorRecord;
+    unsigned long seekRetries;
+    unsigned long seekLength;
 };
 
 void createHeadNodes();
